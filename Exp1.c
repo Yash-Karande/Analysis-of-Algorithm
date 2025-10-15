bc48a4e9e06873bfaf9df7bef586394864281c97
Yash Karande <yash.2406527108@vcet.edu.in>
2025-10-16 00:01:44 +0530

    Update Exp1.c

--- Exp1.c
+++ Exp1.c
@@ -1,49 +1,37 @@
 #include <stdio.h>
 
-void insertionSort(int arr[], int size);
+void insertionSort(int arr[], int size) {
+    for (int j = 1; j < size; j++) {
+        int key = arr[j];
+        int i = j - 1;
+
+        while (i >= 0 && arr[i] > key) {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        arr[i + 1] = key;
+    }
+
+    printf("Sorted array: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 
 int main() {
     int size;
-
-    // Get array size from user
-    printf("\nEnter number of elements: ");
+    printf("Enter the number of elements: ");
     scanf("%d", &size);
 
-    int arr[size];  // Declare array of given size
+    int arr[size];  // Variable Length Array (VLA)
 
-    // Get array elements from user
-    printf("\nEnter %d elements: \n", size);
+    printf("Enter %d elements:\n", size);
     for (int i = 0; i < size; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Display array before sorting
-    printf("\nBefore Sorting:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    // Call insertion sort function
     insertionSort(arr, size);
 
-    // Display array after sorting
-    printf("\n\nAfter Sorting:\n");
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-
     return 0;
-}
-
-void insertionSort(int arr[], int size) {
-    for (int curr = 1; curr < size; curr++) {
-        int prev = curr - 1;
-        int key = arr[curr];
-
-        while (prev >= 0 && arr[prev] > key) {
-            arr[prev + 1] = arr[prev];
-            prev--;
-        }
-        arr[prev + 1] = key;
-    }
 }
